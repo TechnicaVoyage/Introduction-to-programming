805f02fa7e111d07a14c47b4dea84cef4b914aee
TechnicaVoyage <dhavalat60@gmail.com>
2017-01-24 15:25:38 +0530

    C program: Calcuate Simple Interest

---
+++ Simple Interest.c
@@ -0,0 +1,16 @@
+//Simple Interest Calculator
+#include <stdio.h>
+
+void main(){
+	float p,r,t,si;
+	printf("___________________________________________________________________\n");
+	printf("Simple Interest Calculator\n");
+	printf("Enter Principal amount: ");
+	scanf("%f",&p);
+	printf("Enter Rate in percentage: ");
+	scanf("%f",&r);
+	printf("Enter Time period: ");
+	scanf("%f",&t);
+	si = p*(r/100)*t;
+	printf("Simple interest is: %f\n",si);
+}
