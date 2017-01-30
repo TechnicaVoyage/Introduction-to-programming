c8c7cdaf42f5addb73131d4335049136f3da8992
TechnicaVoyage <dhavalat60@gmail.com>
2017-01-31 01:57:43 +0530

    Organized files in folders

--- Source code/First-equation-of-motion.c
+++ Source code/First-equation-of-motion.c
@@ -1,16 +1,23 @@
-//Final speed calculation using v = u + a*t
+//Final speed calculation using v = u + a * t
+
 #include <stdio.h>
 
 void main(){
 	float u,a,t,v;
 	printf("___________________________________________________________________\n");
-	printf("Speed Calculation using v=u+at\n");
+	printf("Speed Calculation using v = u + a*t\n");
+	printf("___________________________________________________________________\n");
+	
 	printf("Enter initial speed(u): ");
 	scanf("%f",&u);
+
 	printf("Enter acceleration(a): ");
 	scanf("%f",&a);
+
 	printf("Enter time(t): ");
 	scanf("%f",&t);
+
 	v = u + (a*t);
+
 	printf("Final speed is: %f\n",v);
 }
--- Source code/Simple-Interest.c
+++ Source code/Simple-Interest.c
@@ -5,12 +5,18 @@ void main(){
 	float p,r,t,si;
 	printf("___________________________________________________________________\n");
 	printf("Simple Interest Calculator\n");
+	printf("___________________________________________________________________\n");
+
 	printf("Enter Principal amount: ");
 	scanf("%f",&p);
+
 	printf("Enter Rate in percentage: ");
 	scanf("%f",&r);
+
 	printf("Enter Time period: ");
 	scanf("%f",&t);
+
 	si = p*(r/100)*t;
+	
 	printf("Simple interest is: %f\n",si);
 }
